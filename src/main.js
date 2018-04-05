02a56a0bc707fc123c5f534a708f51b037e77f05
juanm <juan.musleh@gmail.com>
2018-04-04 20:09:44 -0400

    Movie filters listed (using components

--- src/main.js
+++ src/main.js
@@ -1,1 +1,54 @@
+import Vue from 'vue';
 import './style.scss';
+
+import genres from './util/genres';
+
+new Vue({
+  el: '#app',
+  components: {
+    'movie-list': {
+      template: `<div id="movie-list">
+                  <div v-for="movie in movies" class="movie"> {{ movie.title }} </div>
+                </div>`,
+      data: function (){
+        return {
+            movies: [
+              {title: 'pulp fiction'},
+              {title: 'Home alone'},
+              {title: 'Austin Power'}
+            ]
+          }
+      }
+    },
+    'movie-filter': {
+      data(){
+        return {
+          genres
+        }
+      },
+      template: `<div id="movie-filter">
+                    <h2>Filter Results</h2>
+                    <div class="filter-group">
+                      <check-filter v-for="genre in genres" :key="genre.title" v-bind:title="genre"></check-filter>
+                    </div>
+                 </div>`,
+       // These are only available within movie filter and nowhere else
+      components: {
+        'check-filter': {
+            data() {
+              return {
+                checked: false
+              }
+            },
+            template: `<div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checked = !checked">
+                          <span class="checkbox"></span>
+                          <span clas="check-filter-title">{{ title }}</span>
+                      </div>`,
+            props: ['title'],
+        }
+      }
+    }
+  }
+});
+//components allow you to create your own custom html tags in a way
+//Components are sort of subclasses of Vue (mini instance of view)
