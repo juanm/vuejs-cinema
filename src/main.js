f9ccf4536085bb26a8761176de852f54a1c187a5
juanm <juan.musleh@gmail.com>
2018-04-04 20:35:09 -0400

    Root Vue object tracks what's selected from the filters

--- src/main.js
+++ src/main.js
@@ -5,10 +5,24 @@ import genres from './util/genres';
 
 new Vue({
   el: '#app',
+  data: {
+    genre:[],
+    time:[]
+  },
   methods: {
     checkFilter(category, title, checked) {
       console.log("Root Instance checkFilter");
       console.log(category, title, checked);
+      //Add or remove the filter to the genre filter array
+      if (checked) {
+        //Category could be genre or time, so this[genre] or this[time]
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index > -1) {
+          this[category].splice(index, 1);
+        }
+      }
     }
   },
   components: {
